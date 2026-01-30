a771db277acdd60a8daaa55b205b67224ee58a16
Himanshu Arun Patwardhan <himanshu.arun.patwardhan@gmail.com>
2026-01-30 15:48:56 +0530

    xcode project initial setup

---
+++ Swift-MVVM-Arch/Swift-MVVM-Arch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift-MVVM-Arch
+//
+//  Created by Himanshu Patwardhan on 30/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift-MVVM-Arch/Swift-MVVM-Arch/Swift_MVVM_ArchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_MVVM_ArchApp.swift
+//  Swift-MVVM-Arch
+//
+//  Created by Himanshu Patwardhan on 30/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Swift_MVVM_ArchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
